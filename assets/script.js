e40e27215b185a18f07d484bfb8b0d5b7e86a8e1
aheath <aaron.heath5447@gmail.com>
2023-09-03 19:50:00 -0400

    Add script.js and link in HTML

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (topic of topics) {
+    console.log(topic);
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
